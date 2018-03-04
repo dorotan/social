d40dc00a9376915d880bb9049ff3736e4f76a903
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-03-04 20:30:17 +0100

    Add builtin login method

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
+
 urlpatterns = [
-	#Widok logowania
-	url(r'^login/$', views.user_login, name='login'),
+	#Custom login view
+	# url(r'^login/$', views.user_login, name='login'),
+	#Builtin login view
+	url(r'^login/$', auth_views.login, name='login'),
+	url(r'^logout/$', auth_views.logout, name='logout'),
+	url(r'^logout_then_login/$', auth_views.logout_then_login, name='logout_then_login'),
 ]
