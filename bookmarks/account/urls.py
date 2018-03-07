d80de9fc41b960c7c66cfed2c9092ac0a4979f4c
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-03-07 21:17:43 +0100

    Add reset password methods, urls and html templates

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
 	url(r'^logout/$', auth_views.logout, name='logout'),
 	url(r'^logout_then_login/$', auth_views.logout_then_login, name='logout_then_login'),
 	url(r'^$', views.dashboard, name='dashboard'),
+	url(r'^password_change/$', auth_views.password_change, name='password_change'),
+	url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
 ]
