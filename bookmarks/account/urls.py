52d795b8fa3b66e00406af77926fc23916cad360
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-03-20 21:33:27 +0100

    Add templates and urls for register view

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
 	url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
 	url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
 	url(r'^password_reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+	url(r'^register/$', views.register, name='register'),
 ]
